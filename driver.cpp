379ae122dfdda80923754478663db4fa1bcb2908
Kris Li <krisli210@gmail.com>
2018-12-02 20:59:38 -0600

    now goes through all files and outputs 6 word chunks: ./a.out files/sm_doc_set 6

--- driver.cpp
+++ driver.cpp
@@ -4,8 +4,11 @@
 #include <errno.h>
 #include <vector>
 #include <string>
+#include <cstring>
 #include <iostream>
-
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
 using namespace std;
 
 /*function... might want it in some class?*/
@@ -32,9 +35,38 @@ int main(int argc, char* argv[])
     vector<string> files = vector<string>();
 
     getdir(dir,files);
+    
+    vector<string> sequence;
+    string temp ; 
+
+    int chnk_s = atoi(argv[2]); // Chunk size 
+    cout << "Chunk Size " << chnk_s << endl;
 
     for (unsigned int i = 0;i < files.size();i++) {
         cout << i << files[i] << endl;
+	ifstream myfile;
+
+	if (i > 1) {//skip . and .. 
+            myfile.open((dir + "/" + files[i]).c_str());
+	    if(!myfile.is_open())
+		cout << "file not opened" << endl;
+	    while(myfile >> temp) {
+		sequence.push_back(temp);
+
+		if (sequence.size() == chnk_s) {
+		    //here we hash the vector
+		    for (int i = 0 ; i < sequence.size() ; i++) 
+			cout << sequence[i] << " " ;
+		    cout << endl ;
+
+		    sequence.erase(sequence.begin());
+		}
+	    }
+	myfile.close();	    
+	//cout << "get here" << endl;
+	//return 0 ;
+	}
     }
+     
     return 0;
 }
